11139c02136849d75ec6f84654be1e441462fda6
s-sirakiLab <s-siraki@ist.osaka-u.ac.jp>
2019-04-22 16:48:41 +0900

    first commit

---
+++ src/Board.java
@@ -0,0 +1,14 @@
+import javax.swing.JPanel;
+
+public class Board extends JPanel {
+
+	static final int WIDTH = 500;
+	static final int HEIGHT = 500;
+	static final int OUTER_MARGIN = 50;
+	static final int COMPONENT_SIZE = 50;
+
+	Board(int rows, int cols) {
+
+	}
+
+}
