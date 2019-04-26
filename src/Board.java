ccb46ff4cf8e8ded55c6c61f011a53f1fe2cae63
Shuya Shiraki <s-siraki@ist.osaka-u.ac.jp>
2019-04-26 10:00:48 +0900

    update

--- src/Board.java
+++ src/Board.java
@@ -1,59 +1,59 @@
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
 public class Board extends JPanel {
 
-	static final int WIDTH = 500;
-	static final int HEIGHT = 500;
-	static final int OUTER_MARGIN = 20;
+	static final int WIDTH = 1000;
+	static final int HEIGHT = 1000;
 	private int cols, rows;
 	private int side;
 	private int beginX, beginY, endX, endY;
+	int boardState[][];
 
 	Board(int cols, int rows) {
+		setPreferredSize(new Dimension(WIDTH,HEIGHT));
 		this.cols = cols;
 		this.rows = rows;
+		this.boardState = new int [cols][rows];
 		this.setSide(cols, rows);
 		this.setEdge();
 	}
 
 
 	private void setSide(int cols, int rows) {
-		int rangeX = (WIDTH - OUTER_MARGIN) / cols;
-		int rangeY = (HEIGHT - OUTER_MARGIN) / rows;
+		int rangeX = WIDTH / cols;
+		int rangeY = HEIGHT / rows;
 		if (rangeX < rangeY) this.side = rangeX;
 		else this.side =  rangeY;
 	}
 
 
 	private void setEdge() {
-		this.beginX = OUTER_MARGIN;
-		this.beginY = OUTER_MARGIN;
-		this.endX = this.beginX + side * cols;
-		this.endY = this.endY + side * rows;
+		this.beginX = 0;
+		this.beginY = 0;
+		this.endX = side * cols;
+		this.endY = side * rows;
 	}
-
-
-
 
 
 	// 画面描画
 	public void paintComponent(Graphics g) {
         // 背景
-        g.setColor(Color.gray);
+        g.setColor(Color.lightGray);
         g.fillRect(0, 0, WIDTH, HEIGHT);
         // 盤面描画
         for (int i = 0; i <= this.cols; i++) {
             int x = this.beginX + this.side * i;
             g.setColor(Color.black);
-            g.drawRect(x, x, this.beginY, this.endY);
+            g.drawLine(x, this.beginY, x, this.endY);
         }
         for (int j = 0; j <= this.rows; j++) {
             int y = this.beginY + this.side * j;
             g.setColor(Color.black);
-            g.drawRect(this.beginX, this.endX, y, y);
+            g.drawLine(this.beginX, y, this.endX, y);
         }
     }
 
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+        JFrame f = new JFrame();
+        f.getContentPane().setLayout(new FlowLayout());
+        f.getContentPane().add(new Board(3,3));
+        f.pack();
+        f.setResizable(false);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
 	}
 
 }
